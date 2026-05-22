c481a74d33b760aed50021f3fb234a2a3ce02991
KrishnaKant <krishnaplayz333@gmail.com>
2026-05-22 17:29:13 +0530

    Added Movement and Coords

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 console.log("Script.js Initializing..");
+
+let x = 0;
+let y = 0;
+const index = 15*y + x;
+
 let ap = document.getElementById("auto");
-let up = document.querySelector(".up");
-let down = document.querySelector(".down");
-let left = document.querySelector(".left");
-let right = document.querySelector(".right");
 
 const board = document.getElementById("board");
-
 for (let i = 0; i < 225; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     board.appendChild(cell);
 }
-
 const cells = document.querySelectorAll(".cell");
+cells[index].innerHTML = '<img src="assets/robot-svgrepo-com.svg" alt="robot-icon">';
+
+function robot_placement(){
+    cells.forEach(cell=>{
+        cell.innerHTML="";
+    });
+    const index = 15*y + x;
+    cells[index].innerHTML = '<img src="assets/robot-svgrepo-com.svg" alt="robot-icon">';
+    document.querySelector(".curr").innerHTML = "Current Position : ("+x+","+y+")";
+}
+
+function movement(dir){
+    if(dir === 'up' && y>0)
+        y--;
+    if(dir === 'down' && y<14)
+        y++;
+    if(dir === 'left' && x>0)
+        x--;
+    if(dir === 'right' && x<14)
+        x++;
+    robot_placement();
+}
+
 cells.forEach(cell=>{
     cell.addEventListener("click", ()=>{
         cell.style.backgroundColor = 'var(--sc-color)'; 
     })
-})
+});
+
 
 ap.addEventListener("click", ()=>{
     ap.innerHTML='<span><img src="assets/activated-robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span>'
     ap.lastChild.style.color = 'var(--active-color)'
-})
-
-up.addEventListener("click",()=>{
-    document.getElementById("robot").style.transform = 'translateY(-var(--cell-height))';
-})
-
-down.addEventListener("click",()=>{
-    document.getElementById("robot").style.transform = 'translateY(var(--cell-height))';
-})
-
-left.addEventListener("click",()=>{
-    document.getElementById("robot").style.transform = 'translateX(-var(--cell-height))';
-})
-
-right.addEventListener("click",()=>{
-    document.getElementById("robot").style.transform = 'translateX(var(--cell-height))';
 })
