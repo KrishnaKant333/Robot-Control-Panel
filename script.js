ceb56cf80b4c98ddac74fd4a8e20b32a2105ca9c
KrishnaKant <krishnaplayz333@gmail.com>
2026-05-22 16:32:17 +0530

    square grid fix

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 console.log("Script.js Initializing..");
-let cells = document.querySelectorAll(".cell");
 let ap = document.getElementById("auto");
 let up = document.querySelector(".up");
+let down = document.querySelector(".down");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
 
+const board = document.getElementById("board");
+
+for (let i = 0; i < 225; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
+}
+
+const cells = document.querySelectorAll(".cell");
 cells.forEach(cell=>{
     cell.addEventListener("click", ()=>{
-        cell.setAttribute("style","background-color:var(--sc-color)")
+        cell.style.backgroundColor = 'var(--sc-color)'; 
     })
 })
 
 ap.addEventListener("click", ()=>{
     ap.innerHTML='<span><img src="assets/activated-robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span>'
-    ap.lastChild.setAttribute("style",'color:var(--active-color)')
+    ap.lastChild.style.color = 'var(--active-color)'
 })
 
 up.addEventListener("click",()=>{
-    document.querySelector(".")
+    document.getElementById("robot").style.transform = 'translateY(-var(--cell-height))';
+})
+
+down.addEventListener("click",()=>{
+    document.getElementById("robot").style.transform = 'translateY(var(--cell-height))';
+})
+
+left.addEventListener("click",()=>{
+    document.getElementById("robot").style.transform = 'translateX(-var(--cell-height))';
+})
+
+right.addEventListener("click",()=>{
+    document.getElementById("robot").style.transform = 'translateX(var(--cell-height))';
 })
