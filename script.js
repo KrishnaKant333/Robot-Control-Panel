f4e7722d97ecb281acc00628497dd9263f8e630f
KrishnaKant <krishnaplayz333@gmail.com>
2026-05-26 22:15:58 +0530

    Added AutoPilot and Stop Logic

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ let x = 0;
 let y = 0;
 let battery = 100
 let steps = 0
-steps++
+
+let isStopped = false;
+let isAutoPilot = false;
+let autoPilotInterval;
+
 const index = 15 * y + x;
 
 const ap = document.getElementById("auto");
 const stop = document.getElementById("stop");
 
 const board = document.getElementById("board");
+
+
 for (let i = 0; i < 225; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     board.appendChild(cell);
 }
+
 const cells = document.querySelectorAll(".cell");
+
 cells[index].innerHTML = '<img src="assets/robot-svgrepo-com.svg" alt="robot-icon">';
 
 function robot_placement() {
@@ -28,39 +36,62 @@ function robot_placement() {
 }
 
 function param_update() {
-    document.querySelector(".curr").innerHTML = "Current Position : (" + x + "," + y + ")";
-    document.querySelector(".battery").innerHTML = "Battery Level : " + --battery;
-    document.querySelector(".stp").innerHTML = "Steps Taken : " + steps++;
+    document.querySelector(".curr").innerHTML = `Current Position : (${x}, ${y})`
+    document.querySelector(".battery").innerHTML = `Battery Level : ${battery}`
+    document.querySelector(".stp").innerHTML = `Steps Taken : ${steps}`
 }
 
 function movement(dir) {
-    if (battery > 0) {
-        if (dir === 'up' && y > 0) {
-            document.querySelector(".dir").innerHTML = "Direction Facing : North";
-            y--;
-            param_update();
-        }
-        if (dir === 'down' && y < 14) {
-            document.querySelector(".dir").innerHTML = "Direction Facing : South";
-            y++;
-            param_update();
-        }
-        if (dir === 'left' && x > 0) {
-            document.querySelector(".dir").innerHTML = "Direction Facing : West";
-            x--;
-            param_update();
-        }
-        if (dir === 'right' && x < 14) {
-            document.querySelector(".dir").innerHTML = "Direction Facing : East";
-            x++;
-            param_update();
-        }
-        robot_placement();
+    if (isStopped) {
+        return;
     }
-    else {
-        alert("No Battery!")
+
+    switch (battery) {
+        case 0: {
+            alert("No battery!");
+            clearInterval(autoPilotInterval);
+            isAutoPilot = false;
+            ap.innerHTML = `<span><img src="assets/robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span></span>`;
+            ap.lastElementChild.style.color = "white";
+            break;
+        }
+        case 20: {
+            alert("Low Battery! STOP to recharge");
+            break;
+        }
     }
-} 
+
+    if (battery <= 0) {
+        return;
+    }
+
+    if (dir === 'up' && y > 0) {
+        document.querySelector(".dir").innerHTML = "Direction Facing : North";
+        y--;
+        battery--;
+        steps++;
+    }
+    else if (dir === 'down' && y < 14) {
+        document.querySelector(".dir").innerHTML = "Direction Facing : South";
+        y++;
+        battery--;
+        steps++;
+    }
+    else if (dir === 'left' && x > 0) {
+        document.querySelector(".dir").innerHTML = "Direction Facing : West";
+        x--;;
+        battery--;
+        steps++;
+    }
+    else if (dir === 'right' && x < 14) {
+        document.querySelector(".dir").innerHTML = "Direction Facing : East";
+        x++;
+        battery--;
+        steps++;
+    }
+    robot_placement();
+    param_update();
+}
 
 
 document.addEventListener("keydown", (e) => {
@@ -84,18 +115,67 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
+document.addEventListener("keypress", (e) => {
+    switch (e.key) {
+        case "f":
+            autopilot();
+            break;
+    }
+})
+
 autopilot();
-stopped();
+start_stop();
+
 function autopilot() {
     ap.addEventListener("click", () => {
-        ap.innerHTML = '<span><img src="assets/activated-robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span>'
-        ap.lastChild.style.color = 'var(--active-color)'
-    })
+        if (isStopped) {
+            return;
+        }
+
+        if (isAutoPilot) {
+            clearInterval(autoPilotInterval);
+            isAutoPilot = false;
+            ap.innerHTML = `<span><img src="assets/robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span></span>`;
+            ap.lastElementChild.style.color = "white";
+            return;
+        }
+
+        isAutoPilot = true;
+        ap.innerHTML = `<span><img src="assets/activated-robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span></span>`;
+        ap.lastElementChild.style.color = "var(--active-color)";
+
+        let directions = ['right', 'down', 'left', 'up'];
+        let currDirection = Math.floor(Math.random() * 4);
+        let moveCount = 0;
+
+        autoPilotInterval = setInterval(() => {
+            movement(directions[currDirection]);
+            moveCount++;
+            if (moveCount == 3) {
+                moveCount = 0;
+                currDirection = (currDirection + 1) % 4;
+            }
+        }, 500);
+    });
 }
-function stopped() {
+
+function start_stop() {
     stop.addEventListener("click", () => {
-        stop.innerHTML = '<span><img src="assets/activated-stop-signs-svgrepo-com.svg" alt="stop-sign"></span><span>STOP</span>'
-        stop.lastChild.style.color = 'var(--active-color)'
+        isStopped = !isStopped;
+        if (isStopped) {
+            clearInterval(autoPilotInterval);
+            isAutoPilot = false;
+
+            stop.innerHTML = `<span><img src="assets/activated-stop-signs-svgrepo-com.svg" alt="stop"></span><span>STOPPED</span></span>`;
+            stop.lastElementChild.style.color = "var(--active-color)";
+
+            ap.innerHTML = `<span><img src="assets/robot-svgrepo-com.svg" alt="robot-icon"></span><span>Autopilot Mode</span></span>`;
+            ap.lastElementChild.style.color = "white";
+        }
+        else {
+            stop.innerHTML = `<span><img src="assets/stop-signs-svgrepo-com.svg" alt="stop"></span><span>STOP</span></span>`;
+            stop.lastElementChild.style.color = white;
+        }
     })
 }
 
